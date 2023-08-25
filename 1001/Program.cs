7d900d2c479fa317ca1e0ae1ab1a8438365a4b72
Lucas Ximenes <lucasdximenes@gmail.com>
2023-08-24 21:11:11 -0300

    Add 1001 Solution - C#

---
+++ 1001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int X = A + B;
+
+        Console.WriteLine("X = " + X);
+
+    }
+
+}
