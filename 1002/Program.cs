53fd27dba5ffebdaea546bb9445a71dbc932bf80
Lucas Ximenes <lucasdximenes@gmail.com>
2023-08-24 21:33:13 -0300

    Add 1002 solution - C#

---
+++ 1002/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        double pi = 3.14159;
+        double r = double.Parse(Console.ReadLine());
+        double area = pi * Math.Pow(r, 2);
+
+        Console.WriteLine("A=" + area.ToString("F4"));
+
+    }
+
+}
